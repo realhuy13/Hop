e1d59a7fc6fa880972a4f6d506bd6e4ebe1f3c21
realhuy13 <huy434830@gmail.com>
2025-05-07 19:06:13 +0700

    Add files via upload

---
+++ HopBossMaru.lua
@@ -0,0 +1,47 @@
+local Xeter = 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LuaCrack/Setting/refs/heads/main/427daa95-6994-4738-805e-c1c4c5b577c7.txt"))()
+local A = Xeter:AddWindows()
+local T1 = A:T({ Name = "Hop", })
+
+local I = T1:AddSection("Left", {
+    Name = "Hop"
+})
+
+I:AddButton({
+    Name = "Rip_indra (Wait Update)",
+    Callback = function()
+        setclipboard("")
+    end
+})
+
+I:AddButton({
+    Name = "Dough King (Wait Update)",
+    Callback = function()
+        setclipboard("")
+    end
+})
+
+I:AddButton({
+    Name = "Dark Black (Wait Update)",
+    Callback = function()
+        setclipboard("")
+    end
+})
+
+local I = T1:AddSection("Right", {
+    Name = "Hop Khác"
+})
+
+I:AddButton({
+    Name = "Đảo Bí Ẩn (Wait Update)",
+    Callback = function()
+        setclipboard("")
+    end
+})
+
+I:AddButton({
+    Name = "Full Mon",
+    Callback = function()
+        setclipboard("")
+    end
+})
